bc0d4741b19ba3aeacaf677d5aba27a88c40f89d
Richard Tezbir <tezbir@huglo.com>
2024-08-05 00:18:04 +0200

    mat-table added- designed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'time_counter';
+  dataSource = [
+    
+  ]
+
+  displayedColumns = ['taskname', 'time', 'actions'];
+
+  constructor() { }
 }
